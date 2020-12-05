6a1723f4b23a71172ec4d88f052a79a1247c859a
Valentin <valentine.karnaukhov@gmail.com>
2020-12-05 14:50:33 +0300

    observer

---
+++ src/main/java/com/github/valentinkarnaukhov/observer/Client.java
@@ -0,0 +1,7 @@
+package com.github.valentinkarnaukhov.observer;
+
+/**
+ * @author Valentin Karnaukhov
+ */
+public class Client {
+}
---
+++ src/main/java/com/github/valentinkarnaukhov/observer/EventListener.java
@@ -0,0 +1,10 @@
+package com.github.valentinkarnaukhov.observer;
+
+/**
+ * @author Valentin Karnaukhov
+ */
+public interface EventListener {
+
+    void notify(Object data);
+
+}
